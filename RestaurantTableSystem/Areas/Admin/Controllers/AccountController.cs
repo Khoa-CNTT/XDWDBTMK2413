4c37e0ce10c5537126db6c69c8166dae40927395
duythang160603 <ngo.thang1606#gmail,com>
2025-04-12 11:55:01 +0700

    fix

---
+++ RestaurantTableSystem/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using RestaurantTableSystem.Models;
+
+namespace RestaurantTableSystem.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private RestaurantTableSystemEntities db = new RestaurantTableSystemEntities();
+
+        //public ActionResult UserList()
+        //{
+        //    var users = db.Users.ToList(); // Lấy toàn bộ user
+        //    return View(users);
+        //}
+        // GET: Admin/Account
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList(); // Lấy toàn bộ user
+            return View(users);
+        }
+    }
+}
---
+++ RestaurantTableSystem/Areas/Admin/Controllers/RestaurantCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantTableSystem.Models;
+
+namespace RestaurantTableSystem.Areas.Admin.Controllers
+{
+    public class RestaurantCategoryController : Controller
+    {
+        private RestaurantTableSystemEntities db = new RestaurantTableSystemEntities();
+        // GET: Admin/RestaurantCategory
+        public ActionResult Index()
+        {
+            var restaurant = db.Restaurants.ToList(); // Lấy toàn bộ restaurant
+            return View(restaurant);
+        }
+    }
+}
